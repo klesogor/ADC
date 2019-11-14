21e832dabab7668aed091e7015bc8b989535a58c
Yevheniy <31356150+dzeko14@users.noreply.github.com>
2019-11-14 08:37:18 +0200

    Add adc, use it with buffer (#2)
    * Add adc, use it with buffer
    
    * Fixed linking
    
    Fixed this stuff, works now
    
    * Using avg insted of next

---
+++ Objects/buffer.d
@@ -0,0 +1,4 @@
+.\objects\buffer.o: buffer.c
+.\objects\buffer.o: buffer.h
+.\objects\buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\buffer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
